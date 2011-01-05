59acc6a2dd1a3ef941eec48d4551117cd0db3d97
Michael Warkentin <mwarkentin@gmail.com>
2011-01-05 08:45:33 -0500

    extra credit - added height in cm and weight in kg

--- ex5.py
+++ ex5.py
@@ -1,14 +1,16 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie
 height = 74 # inches
+height_in_cm = height * 2.54
 weight = 180 # lbs
+weight_in_kg = weight * 0.45359237
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %r." % name
-print "He's %r inches tall." % height
-print "He's %r pounds heavy." % weight
+print "He's %r inches tall (%r cm)." % (height, height_in_cm)
+print "He's %r pounds heavy (%r kg)." % (weight, weight_in_kg)
 print "Actually that's not too heavy."
 print "He's got %r eyes and %r hair." % (eyes, hair)
 print "His teeth are usually %r depending on the coffee." % teeth
