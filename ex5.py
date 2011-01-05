c3d37bb7cc343b0f46cad9d632c1502ebd2288df
Michael Warkentin <mwarkentin@gmail.com>
2011-01-05 08:39:06 -0500

    extra credit - replaced all format characters with %r

--- ex5.py
+++ ex5.py
@@ -6,13 +6,13 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
-print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "Let's talk about %r." % name
+print "He's %r inches tall." % height
+print "He's %r pounds heavy." % weight
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (eyes, hair)
-print "His teeth are usually %s depending on the coffee." % teeth
+print "He's got %r eyes and %r hair." % (eyes, hair)
+print "His teeth are usually %r depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (
+print "If I add %r, %r, and %r I get %r." % (
 	age, height, weight, age + height + weight)
