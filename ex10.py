0f12311ba20e5b742befa7f12b4471d2766fd2e9
Michael Warkentin <mwarkentin@gmail.com>
2011-01-08 16:21:01 -0500

    added extra credit - 1 & 2

--- ex10.py
+++ ex10.py
@@ -13,3 +13,6 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+# 1. All python escape sequences: \\, \', \", \a, \b, \f, \n, \N{name}, \r, \t, \uxxxx, \Uxxxxxxxx, \v, \ooo, \xhh
+# 2. If you were printing python code, you'd need to use ''' if you were printing a long python string starting with """
