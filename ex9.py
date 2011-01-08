4523220fb99762c2334398f166a25b899293b0cb
Michael Warkentin <mwarkentin@gmail.com>
2011-01-08 16:07:55 -0500

    added example 9

---
+++ ex9.py
@@ -0,0 +1,13 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here's the days: ", days
+print "Here's the months: ", months
+
+print """
+There's somethings going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+"""
