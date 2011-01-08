3315168ea0ad81ba323a5fd8329dd9c377f8f138
Michael Warkentin <mwarkentin@gmail.com>
2011-01-08 16:51:57 -0500

    added extra credit

--- ex11.py
+++ ex11.py
@@ -1,3 +1,5 @@
+print "What's your name?"
+name = raw_input('==>')
 print "How old are you?",
 age = raw_input()
 print "How many tall are you?",
@@ -5,5 +7,7 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
+print "So, your name is %r, you're %r old, %r tall and %r heavy." % (
+    name, age, height, weight)
+    
+# 1. raw_input() takes input from the user, and always returns a string
