b8ddf6bb12690dd69eb221e6050ea6ade33d20f0
Michael Warkentin <mwarkentin@gmail.com>
2011-01-08 17:08:58 -0500

    added answer to first extra credit

--- ex13.py
+++ ex13.py
@@ -6,3 +6,7 @@ print "The script is called:", script
 print "The first variable is:", first
 print "The second variable is:", second
 print "Your third variable is:", third
+
+# Extra credit
+# 1. Error: ValueError: need more than 2 values to unpack
+# This means that the script needs the correct amount of arguments (3) to process correctly
