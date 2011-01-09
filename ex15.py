37594837122ec0b2176532437d5ae25a93c68d17
Michael Warkentin <mwarkentin@gmail.com>
2011-01-09 11:38:54 -0500

    back to both methods

--- ex15.py
+++ ex15.py
@@ -1,3 +1,18 @@
+# import argv from the sys module
+from sys import argv
+
+# set script and filename variables from the script arguments
+script, filename = argv
+
+# open the filename passed in as an argument, and assign the file object to the txt variable
+txt = open(filename)
+
+# print a string with the filename
+print "Here's your file %r:" % filename
+
+# print the contexts of the txt file object
+print txt.read()
+
 # print a string
 print "I'll also ask you to type it again:"
 
