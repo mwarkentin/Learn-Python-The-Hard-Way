f4f364f33b0e170c20e4c4ac7353fa7f1431f9c8
Michael Warkentin <mwarkentin@gmail.com>
2011-01-09 16:51:14 -0500

    extra credit - removed extra print statements and user prompt

--- ex17.py
+++ ex17.py
@@ -9,13 +9,5 @@ print "Copying from %s to %s" % (from_file, to_file)
 input = open(from_file)
 indata = input.read()
 
-print "The input file is %d bytes long" % len(indata)
-
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C (^C) to abort."
-raw_input()
-
 output = open(to_file, 'w')
 output.write(indata)
-
-print "Alright, all done."
