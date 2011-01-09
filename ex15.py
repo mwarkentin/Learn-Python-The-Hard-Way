46d377a1c862eb1f370847f84d1a0d037c42d574
Michael Warkentin <mwarkentin@gmail.com>
2011-01-09 11:37:21 -0500

    extra credit - script with first method of file choice only

--- ex15.py
+++ ex15.py
@@ -12,15 +12,3 @@ print "Here's your file %r:" % filename
 
 # print the contexts of the txt file object
 print txt.read()
-
-# print a string
-print "I'll also ask you to type it again:"
-
-# prompt the user for a file name
-file_again = raw_input("> ")
-
-# open the file that the user entered, and then assign the file object to the variable txt_again
-txt_again = open(file_again)
-
-# print the contents of the txt_again file object
-print txt_again.read()
