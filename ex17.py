466849b9c366118566e6b014d89f2e264b0df66d
Michael Warkentin <mwarkentin@gmail.com>
2011-01-09 16:54:19 -0500

    turned script into 1 line (plus imports and argv parsing)

--- ex17.py
+++ ex17.py
@@ -3,11 +3,4 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
-
-# we could do these two on one line too, how?
-input = open(from_file)
-indata = input.read()
-
-output = open(to_file, 'w')
-output.write(indata)
+open(to_file, 'w').write(open(from_file).read())
