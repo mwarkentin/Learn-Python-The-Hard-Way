c9d881a0909cd2142fcca9ec3e5ce0e38dfdbd74
Michael Warkentin <mwarkentin@gmail.com>
2011-01-09 22:59:05 -0500

    added comments explaining puzzle

--- ex21.py
+++ ex21.py
@@ -28,6 +28,8 @@ print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 # A puzzle for the extra credit, type it in anyway
 print "Here is a puzzle."
 
+# age + height - weight * (iq / 2)
+# 35  + 74     - 180    * (50 / 2)
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
