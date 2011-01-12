5c41267c7b7f79b990ee7d91cb69613ce883b7e2
Michael Warkentin <mwarkentin@gmail.com>
2011-01-12 18:34:11 -0500

    added answers for exercise 28

---
+++ ex28.py
@@ -0,0 +1,20 @@
+# 1. True
+# 2. False
+# 3. False
+# 4. True
+# 5. True
+# 6. True
+# 7. False
+# 8. True
+# 9. False
+#10. False
+#11. True
+#12. False
+#13. True
+#14. False
+#15. False
+#16. False
+#17. True
+#18. True
+#19. False
+#20. False
