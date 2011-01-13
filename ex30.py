4e627a66463959850f1dcb1863f49d4172c3595b
Michael Warkentin <mwarkentin@gmail.com>
2011-01-13 08:29:34 -0500

    added exercise 30

---
+++ ex30.py
@@ -0,0 +1,23 @@
+people = 30
+cars = 40
+buses = 15
+
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+    
+if buses > cars:
+    print "that's too many buses."
+elif buses < cars:
+    print "Maybe we could take the buses."
+else:
+    print "We still can't decide."
+    
+if people > buses:
+    print "Alright, let's just take the buses."
+else:
+    print "Fine, let's stay home then."
