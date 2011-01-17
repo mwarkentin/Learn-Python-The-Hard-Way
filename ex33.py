84e8a351a7c5534901c5ff4703e781d4657ae6aa
Michael Warkentin <mwarkentin@gmail.com>
2011-01-17 11:44:32 -0500

    extra credit - added step option to add_numbers function

--- ex33.py
+++ ex33.py
@@ -1,17 +1,17 @@
-def add_numbers(max):
+def add_numbers(max, step=1):
     i = 0
     numbers = []
     while i < max:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + step
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
     
     return numbers
 
-numbers = add_numbers(10)
+numbers = add_numbers(10, 5)
 
 print "The numbers: "
 
