9c4f5d0dce6c8b1a11d502f30ed51cb7aea27fa4
Michael Warkentin <mwarkentin@gmail.com>
2011-01-17 11:28:04 -0500

    extra credit - replaced while loop with a function

--- ex33.py
+++ ex33.py
@@ -1,14 +1,17 @@
-i = 0
-numbers = []
+def add_numbers(max):
+    i = 0
+    numbers = []
+    while i < max:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
     
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    return numbers
 
+numbers = add_numbers(10)
 
 print "The numbers: "
 
