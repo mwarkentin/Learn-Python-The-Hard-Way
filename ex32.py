c44a97e2ab96aa163334b93312e5ccb70fba71a4
Michael Warkentin <mwarkentin@gmail.com>
2011-01-17 11:06:19 -0500

    extra credit - replaced for loop with direct assignment

--- ex32.py
+++ ex32.py
@@ -19,10 +19,7 @@ for i in change:
 elements = []
 
 # then use the range function to do 0 to 20 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
-    # append is a function that lists understand
-    elements.append(i)
+elements = range(0, 6)
     
 # now we can print them out too
 for i in elements:
