1eff1b0281308b825558e690a7c14b2d93ec6c52
cclauss <cclauss@bluewin.ch>
2017-05-13 07:54:08 +0200

    Gracefully deal with both versions of Python

--- ex13-raw_input.py
+++ ex13-raw_input.py
@@ -1,5 +1,10 @@
 from sys import argv
 
+try:  # https://docs.python.org/3/whatsnew/3.0.html#builtins
+    raw_input          # Python 2
+except NameError:
+    raw_input = input  # Python 3
+
 script, first_name, last_name = argv
 
 middle_name = raw_input("What is your middle name? ")
